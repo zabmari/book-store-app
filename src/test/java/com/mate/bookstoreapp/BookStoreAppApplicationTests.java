cf8bc2fa282b8c5908be8fe00396b29311638e7d
Marika Żaboklicka <marika.zaboklicka@wp.pl>
2025-06-06 11:35:07 +0200

    infrastructure changes

---
+++ src/test/java/com/mate/bookstoreapp/BookStoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mate.bookstoreapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
